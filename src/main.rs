6715c0df2334598a3ef0570cfcf018f4c19624b0
shalev <shalevari2008@gmail.com>
2024-02-10 15:46:55 +0200

    feat(license-file): check if license file already exists and act accordingly

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use dialoguer::{theme::ColorfulTheme, Select};
-use std::fs::File;
-use std::io::prelude::*;
+use dialoguer::{theme::ColorfulTheme, Confirm, Select};
+use std::fs::{self, File};
+use std::io::{self, prelude::*};
 
 fn main() {
     let licenses = ["MIT", "Apache-2.0", "GPL-3.0"];
@@ -21,14 +21,28 @@ fn main() {
         _ => panic!("Invalid license selected."),
     };
 
-    if let Err(err) =
-        File::create("LICENSE").and_then(|mut file| file.write_all(license_text.as_bytes()))
-    {
-        eprintln!("Error creating LICENSE file: {}", err);
-    } else {
-        println!(
-            "A LICENSE file for the {} license was successfully created!",
-            selected_license
-        );
+    let license_file_path = "LICENSE";
+
+    if fs::metadata(license_file_path).is_ok() {
+        // License file already exists
+        let replace_existing = Confirm::with_theme(&ColorfulTheme::default())
+            .with_prompt("A LICENSE file already exists. Do you want to replace it?")
+            .interact()
+            .unwrap();
+
+        if !replace_existing {
+            println!("Operation canceled. No changes made.");
+            return;
+        }
+    }
+
+    match File::create(license_file_path).and_then(|mut file| file.write_all(license_text.as_bytes())) {
+        Ok(_) => {
+            println!(
+                "A LICENSE file for the {} license was successfully created!",
+                selected_license
+            );
+        }
+        Err(err) => eprintln!("Error creating LICENSE file: {}", err),
     }
 }
